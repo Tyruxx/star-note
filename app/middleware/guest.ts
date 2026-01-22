f38564b556a972e517aa51bbe407916acba7d886
Alan Sebastian <alansbm88@gmail.com>
2026-01-22 15:37:35 +0800

    fix oauth

--- app/middleware/guest.ts
+++ app/middleware/guest.ts
@@ -1,11 +1,11 @@
 import { useAppwrite } from "~/composables/useAppwrite"
 
 export default defineNuxtRouteMiddleware(async () => {
-  try {
-    const { account } = useAppwrite()
-    await account.get()
-    return navigateTo('/')
-  }
-  catch (err) {
-  }
+//   try {
+//     const { account } = useAppwrite()
+//     await account.get()
+//     return navigateTo('/')
+//   }
+//   catch (err) {
+//   }
 })
