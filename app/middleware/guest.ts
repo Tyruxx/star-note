e6b6cfceaa6b1260f7c6964231aaac6815217110
Alan Sebastian <alansbm88@gmail.com>
2026-01-22 22:15:47 +0800

    debug oauth

--- app/middleware/guest.ts
+++ app/middleware/guest.ts
@@ -3,11 +3,13 @@ import { useAppwrite } from "~/composables/useAppwrite"
 export default defineNuxtRouteMiddleware(async () => {
   const { account } = useAppwrite()
   try {
-    await account.getSession({
+    const session = await account.getSession({
       sessionId: 'current'
     })
+    console.log(session.provider);
     return navigateTo('/')
   }
   catch (error) {
+    console.log(error)
   }
 })
