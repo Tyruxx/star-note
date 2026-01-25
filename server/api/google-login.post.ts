359466b6fe5b1852aa52922c137bb14438ecd030
Alan Sebastian <alansbm88@gmail.com>
2026-01-26 04:32:11 +0800

    debug supabase

--- server/api/google-login.post.ts
+++ server/api/google-login.post.ts
@@ -9,7 +9,7 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     const token = body.token
 
-    if (!token) {
+    if (token) {
         throw createError({
             statusCode: 400,
             statusMessage: 'No token or invalid token provided'
