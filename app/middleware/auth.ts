9563c882d1d2ce3efe388f9f1ec232db0b5f3a2d
Alan Sebastian <alansbm88@gmail.com>
2026-01-26 18:08:41 +0800

    fix supabase

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((from) => {
-    const { loggedIn } = useUserSession()
+    // const { loggedIn } = useUserSession()
     
-    if (!loggedIn.value) {
-        return navigateTo('/')
-    }
+    // if (!loggedIn.value) {
+    //     return navigateTo('/')
+    // }
 })
