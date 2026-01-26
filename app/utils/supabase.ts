63c90083fc3ba0e9b0c33e3383f1ab834a82854f
Alan Sebastian <alansbm88@gmail.com>
2026-01-26 17:18:21 +0800

    fix supabase

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from './../../database.types';
+import type { Database } from '../types/database.types'
 
 const config = useRuntimeConfig()
 const supabaseUrl = config.public.supabaseUrl;
